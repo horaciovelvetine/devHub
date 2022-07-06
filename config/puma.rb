3d1cbee5f492bb2706cafe40714777a305baeaef
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2022-07-06 15:37:56 -0600

    add comments for prep in puma (for prod)

--- config/puma.rb
+++ config/puma.rb
@@ -30,6 +30,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
+##! @horaciovelvetine un-comment for production-prep
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
@@ -37,6 +38,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
+##! @horaciovelvetine un-comment for production-prep
 # preload_app!
 
 # Allow puma to be restarted by `bin/rails restart` command.
