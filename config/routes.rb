3cdcbc643545b1a1019cb2fc1cd641c57c7a9155
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2022-07-06 08:32:33 -0600

    init original routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # ! QUEBERT
+  scope module: 'quebert', path: 'api/quebert' do
+    ## ALL Routines & Scheduled Tasks
+    get 'post-routine', to: 'routines#que_routine'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+    ## ALL Slash commands route through here
+    get 'cronfig', to: 'commands#cronfig'
+    post 'cronfig', to: 'commands#cronfig'
+    post 'slash-command', to: 'commands#slash_command'
+  end
+
+  # ! THE_OREGON_TRAIL
+  scope module: 'oregon_trail', path: 'api/oregon-trail' do
+    resources :games
+  end
 end
